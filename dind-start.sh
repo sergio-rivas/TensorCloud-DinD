30862b392e4d436ab2d897151f9f735de722ef89
Sergio Rivas <sergiorivas@163.com>
2018-08-16 17:10:02 +0800

    add self-removal

--- dind-start.sh
+++ dind-start.sh
@@ -14,10 +14,11 @@ pieces=( dind-1.sh dind-2.sh dind-3.sh dind-4.sh dind-5.sh )
 
 for i in "${pieces[@]}"
 do
-  wget -P temp/ https://bashing.oss-cn-hangzhou.aliyuncs.com/dind/$i
+  wget -P temp/ -q https://bashing.oss-cn-hangzhou.aliyuncs.com/dind/$i
   cat temp/$i >> run.sh
   rm temp/$i
 done
 
 bash run.sh
 rm run.sh
+rm dind-start.sh
